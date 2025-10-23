d10be8ee19ff3fd90564142e33b8d94cca6180cd
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-10-23 14:33:53 +1300

    test: make JSON pretty

--- tools/apitester/helpers.go
+++ tools/apitester/helpers.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/tidwall/pretty"
 	"gopkg.in/dnaeon/go-vcr.v4/pkg/cassette"
 )
 
@@ -57,7 +58,7 @@ func readBody(t *testing.T, resp *http.Response) string {
 		t.Fatal(err)
 	}
 
-	return string(body)
+	return string(pretty.Pretty(body))
 }
 
 func PlayInteraction(t *testing.T, interaction *cassette.Interaction) *http.Response {
